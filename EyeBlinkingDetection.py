25dba864912a0bf38a9fb874d0bfa4a514d1d57f
qianyi wang <qianyiwang@qianyis-MacBook-Pro.local>
2017-02-01 21:45:33 -0500

    using BlobDetector

--- EyeBlinkingDetection.py
+++ EyeBlinkingDetection.py
@@ -11,6 +11,29 @@ cap = cv2.VideoCapture(0)
 windowClose = np.ones((5,5),np.uint8)
 windowOpen = np.ones((2,2),np.uint8)
 windowErode = np.ones((2,2),np.uint8)
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+# Change thresholds
+# params.minThreshold = 10;
+# params.maxThreshold = 200;
+ 
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 500.0;
+# params.maxArea = 1500.0;
+ 
+# Filter by Circularity
+# params.filterByCircularity = True
+# params.minCircularity = 0.1
+ 
+# Filter by Convexity
+# params.filterByConvexity = True
+# params.minConvexity = 1
+ 
+# Filter by Inertia
+# params.filterByInertia = True
+# params.minInertiaRatio = 0.01
+detector = cv2.SimpleBlobDetector(params)
 while True:
 	ret, img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -30,36 +53,19 @@ while True:
 			eyeImg_gray = roi_gray[ey:ey+eh, ex:ex+ew]
 			eyeImg_color = roi_color[ey:ey+eh, ex:ex+ew]
 			pupilFrame = cv2.equalizeHist(eyeImg_gray)
-			pupilO = pupilFrame
 			ret, pupilFrame = cv2.threshold(pupilFrame,55,255,cv2.THRESH_BINARY)#50 ..nothin 70 is better
 			pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_CLOSE, windowClose)
 			pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_ERODE, windowErode)
 			pupilFrame = cv2.morphologyEx(pupilFrame, cv2.MORPH_OPEN, windowOpen)
 			#now we find the biggest blob and get the centriod
-			threshold = cv2.inRange(pupilFrame,250,255) #get the blobs
-			contours, hierarchy = cv2.findContours(threshold,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+			blobs = detector.detect(pupilFrame)
+			# Draw detected blobs as red circles.
+			im_with_blobs = cv2.drawKeypoints(eyeImg_color, blobs, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-			maxArea = 0
-			for cnt in contours:
-				area = cv2.contourArea(cnt)
-				if area > maxArea:
-					maxArea = area
-					largeBlob = cnt
-					
-			if len(largeBlob) > 1:	
-				center = cv2.moments(largeBlob)
-				cx,cy = int(center['m10']/center['m00']), int(center['m01']/center['m00'])
-				cv2.circle(eyeImg_color,(cx,cy),5,(0,0,255),-1)
-				eyeCloseFlag = False
-
-		if len(eyes)<2:
-			eyeCloseFlag = True
-	if eyeCloseFlag==False and len(faces)>0:
-		cv2.putText(img, 'Eye Open', (10,500), font, 3,(255,0,0),2)
-	elif eyeCloseFlag==True and len(faces)>0:
-		cv2.putText(img, 'Eye Closed', (10,500), font, 3,(0,0,255),2)
+			
 	# show the screen
 	cv2.imshow('img', img)
+	cv2.imshow('im_with_blobs',im_with_blobs)
 	# stop by pressing esc
 	k = cv2.waitKey(30) & 0xff
 	if k==27:
